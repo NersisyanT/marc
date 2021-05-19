c50558099cdc9018c0a0a21c17a61a53b4916bbd
NersisyanT <nersisuan1999@gmail.com>
2021-05-19 18:24:09 +0300

    initial commit

--- skins/src/pages/Main.vue
+++ skins/src/pages/Main.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="scrollme first-section">
-        <div class="main_parallax" style="background-image: url({THEME}/img/back2.jpg);"></div>
+        <div class="main_parallax"></div>
         <div class="parallax-component_image">
             <div  class="animateme wow fadeIn" data-when="span" data-from="0" data-to="0.3" data-easing="easeinout" data-opacity="0" data-scale="0.95" data-wow-duration="1000ms" data-wow-delay="0ms" style="opacity: 0; visibility: hidden;">
                 <div class="logo">
@@ -22,7 +22,14 @@
 
 <script>
     export default {
-}
+        data() {
+            return {
+                images: {
+                    image: require('@/img/back2.jpg')
+                }
+            }
+        }
+    }
 </script>
 
 <style lang="scss">
